4cd6a3e872feef8fd22d33669a034ce387abbb0a
Sadikcserymec077 <sadik.cse.rymec@gmail.com>
2026-08-17 06:32:09 +0530

    Time: 0 ms (100%), Space: 43.6 MB (61.95%) - LeetHub

--- 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.java
+++ 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.java
@@ -1,18 +1,20 @@
 class Solution {
-    public int countElements(int[] nums) 
-    {
-        int total = 0;
-        Arrays.sort(nums);
-        int min = nums[0];
-        int n = nums.length;
-        int max = nums[n-1];
-        for(int i=1;i<n-1;i++)
-        {
-            if(nums[i] > min && nums[i] < max)
-            {
-                total++;
-            }
-        }  
-        return total;
+    public int countElements(int[] nums) {
+        int max=Integer.MIN_VALUE;
+        int min=Integer.MAX_VALUE;
+        int k=0;
+        for(int num: nums) 
+        { 
+            max=Math.max(max,num); 
+            min=Math.min(min,num); 
+        }
+        for(int num: nums) 
+        { 
+            if(num!=max && num!=min) 
+            { 
+                k++; 
+            }  
+        }
+        return k;
     }
 }
