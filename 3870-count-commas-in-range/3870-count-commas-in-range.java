4f9182a4feb65d1e2ae8b399004af4be788a2c5c
Sadikcserymec077 <sadik.cse.rymec@gmail.com>
2026-09-08 22:51:09 +0530

    Time: 1 ms (83.65%), Space: 42.6 MB (39.85%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,14 +1,7 @@
 class Solution {
     public int countCommas(int n) {
-        long answer = 0;
-        long threshold = 1000;
-
-        while (threshold <= n) {
-            answer += n - threshold + 1;
-
-            threshold *= 1000;
-        }
-
-        return (int) answer;
+        if(n < 1000)
+            return 0;
+        return (n - 1000) + 1;
     }
 }
